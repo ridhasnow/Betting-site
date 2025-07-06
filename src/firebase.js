1320ac62b854498cff1e6a73ef76cf7f4d94aa09
ridhasnow <threshsupprt@gmail.com>
2025-07-06 16:46:51 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyApBWOINYGbS3cHPzN8Vzy-XMEztueR5Ek",
   authDomain: "cazabet-bd515.firebaseapp.com",
   projectId: "cazabet-bd515",
-  storageBucket: "cazabet-bd515.firebasestorage.app",
+  storageBucket: "cazabet-bd515.appspot.com", // ← صحح هنا فقط
   messagingSenderId: "815113045261",
   appId: "1:815113045261:web:9dd24f0897a5bcf73d5c44",
   measurementId: "G-C1NQ3P9HRL"
 };
 
-// تهيئة التطبيق وربطه بفايرباز
 const app = initializeApp(firebaseConfig);
-
-// تهيئة قاعدة البيانات Firestore
 export const db = getFirestore(app);
