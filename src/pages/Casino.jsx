8d67f05034009966ac62ddf1724d3841a40b2ec8
ridhasnow <threshsupprt@gmail.com>
2025-07-10 00:53:13 +0200

    Update Casino.jsx

--- src/pages/Casino.jsx
+++ src/pages/Casino.jsx
@@ -1,1 +1,12 @@
-a
+import React from "react";
+import Slot5LionsGame from "../casino-games/slot-5lions/Slot5LionsGame";
+
+const Casino = () => (
+  <div>
+    <h1>🎰 ألعاب الكازينو</h1>
+    <Slot5LionsGame />
+    {/* لاحقًا أضف ألعاب أخرى هنا */}
+  </div>
+);
+
+export default Casino;
